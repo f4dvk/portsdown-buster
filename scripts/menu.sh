efb4c427aab330d191075f4695399d4b4f7259ef
f4dvk <stephanef4dvk@gmail.com>
2019-02-08 19:22:26 +0100

    Update menu.sh

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -696,7 +696,6 @@ do_output_setup_mode()
       :
     ;;
     LIMEMINI)
-      FREQ_OUTPUT=$(get_config_var freqoutput $PCONFIGFILE)
       LIME_GAIN=$(get_config_var limegain $PCONFIGFILE)
       GAIN=$(whiptail --inputbox "$StrLimeGainContext" 8 78 $LIME_GAIN --title "$StrLimeGainTitle" 3>&1 1>&2 2>&3)
       if [ $? -eq 0 ]; then
