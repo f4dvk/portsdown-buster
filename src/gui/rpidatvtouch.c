9234e80998b07d3a0b1e3c5468e402f8462faea8
f4dvk <stephanef4dvk@gmail.com>
2019-02-19 22:08:24 +0100

    Update rpidatvtouch.c

--- src/gui/rpidatvtouch.c
+++ src/gui/rpidatvtouch.c
@@ -11858,14 +11858,12 @@ void waituntil(int w,int h)
           printf("RTLSDR\n");
 	  CurrentMenu=5;
 	  Start_Highlights_Menu5();
-	  UpdateWindow();
           break;
         case 6:                               // LIMEMINI
           SetConfigParam(PATH_RXPRESETS, "rx0sdr", "LIMEMINI");
           printf("LIMEMINI\n");
 	  CurrentMenu=5;
           Start_Highlights_Menu5();
-	  UpdateWindow();
           break;
         default:
           printf("Menu 39 Error\n");
