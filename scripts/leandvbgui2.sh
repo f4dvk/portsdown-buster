5a928c196b31d015d4ab735be9404c5cf4bec1b3
f4dvk <stephanef4dvk@gmail.com>
2019-03-09 12:00:42 +0100

    Update leandvbgui2.sh

--- scripts/leandvbgui2.sh
+++ scripts/leandvbgui2.sh
@@ -44,14 +44,19 @@ SAMPLERATEK=$(get_config_var rx0samplerate $RXPRESETSFILE)
 if [ "$SAMPLERATEK" = "0" ]; then
   if [ "$SYMBOLRATEK" -lt 251 ]; then
     SR_RTLSDR=300000
-  elif [ "$SYMBOLRATEK" -gt 250 ] && [ "$SYMBOLRATEK" -lt 1000 ]; then
+  elif [ "$SYMBOLRATEK" -gt 250 ] && [ "$SYMBOLRATEK" -lt 500 ] && [ "$SDR" = "RTLSDR" ]; then
+    SR_RTLSDR=1000000
+  elif [ "$SYMBOLRATEK" -gt 499 ] && [ "$SYMBOLRATEK" -lt 1000 ]; then
     SR_RTLSDR=1200000
   elif [ "$SYMBOLRATEK" -gt 999 ] && [ "$SYMBOLRATEK" -lt 1101 ]; then
     SR_RTLSDR=1250000
   else
     SR_RTLSDR=2400000
   fi
-else
+  if [ "$SYMBOLRATEK" -gt 250 ] && [ "$SYMBOLRATEK" -lt 500 ] && [ "$SDR" = "LIMEMINI" ]; then
+    SR_RTLSDR=850000
+  fi  
+  else
   let SR_RTLSDR=SAMPLERATEK*1000
 fi
 
