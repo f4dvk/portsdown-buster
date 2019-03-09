c6e983ad5711291c89abf9b069ac30bea571b41e
f4dvk <stephanef4dvk@gmail.com>
2019-03-09 19:14:07 +0100

    Update b.sh

--- scripts/b.sh
+++ scripts/b.sh
@@ -73,6 +73,9 @@ MODE_OUTPUT=$(get_config_var modeoutput $PCONFIGFILE)
 
   # And make sure limesdr_send has been stopped
   sudo killall -9 limesdr_send >/dev/null 2>/dev/null
+  if [ "$MODE_OUTPUT" == "LIMEMINI" ]; then
+   /home/pi/rpidatv/bin/limesdr_stopchannel
+  fi
 
   # Stop the audio for CompVid mode
   sudo killall arecord >/dev/null 2>/dev/null
