610f3f063b09df5544eed5d0cbc14b53acd38e86
f4dvk <stephanef4dvk@gmail.com>
2020-02-20 22:03:36 +0100

    202002142 Correct LimeDVB Power Level at 1MS

--- scripts/a.sh
+++ scripts/a.sh
@@ -425,6 +425,13 @@ IDRPERIOD=100
 # Set the SDR Up-sampling rate and correct gain for Lime Modes
 case "$MODE_OUTPUT" in
   "LIMEMINI" | "LIMEUSB" | "LIMEDVB" | "JLIME" | "JEXPRESS")
+
+    # Make sure Lime gain is sensible
+    if [ "$LIME_GAIN" -lt 6 ]; then
+      LIMEGAIN=6
+    fi
+
+    # Set digital gain
     let DIGITAL_GAIN=($LIME_GAIN*31)/100 # For LIMEDVB
 
     if [ "$SYMBOLRATE_K" -lt 990 ] ; then
@@ -435,32 +442,32 @@ case "$MODE_OUTPUT" in
       LIME_GAINF=`echo - | awk '{print '$LIME_GAIN' / 100}'`
     elif [ "$UPSAMPLE" == "1" ]; then
 #      UPSAMPLE=1
-      if [ "$LIME_GAIN" -lt 6 ]; then
-        LIMEGAIN=6
-      fi
+      LIME_GAINF=`echo - | awk '{print ( '$LIME_GAIN' - 6 ) / 100}'`
+    fi
+
+    # Equalise Carrier Mode Level for Lime
+    if [ "$MODE_INPUT" == "CARRIER" ]; then
       LIME_GAINF=`echo - | awk '{print ( '$LIME_GAIN' - 6 ) / 100}'`
     fi
 
     # Override for LIMEDVB Mode
     if [ "$MODE_OUTPUT" == "LIMEDVB" ]; then
+
+      # For Low SRs
       UPSAMPLE=4
+      LIME_GAINF=`echo - | awk '{print '$LIME_GAIN' / 100}'`
+
       if [ "$SYMBOLRATE_K" -gt 1010 ] ; then
         UPSAMPLE=2
+        LIME_GAINF=`echo - | awk '{print ( '$LIME_GAIN' - 6 ) / 100}'`
       fi
       if [ "$SYMBOLRATE_K" -gt 4010 ] ; then
         UPSAMPLE=1
+        LIME_GAINF=`echo - | awk '{print ( '$LIME_GAIN' - 6 ) / 100}'`
       fi
       CUSTOM_FPGA="-F"
     else
       CUSTOM_FPGA=" "
-    fi
-
-    # Equalise Carrier Mode Level for Lime
-    if [ "$MODE_INPUT" == "CARRIER" ]; then
-      if [ "$LIME_GAIN" -lt 6 ]; then
-        LIMEGAIN=6
-      fi
-      LIME_GAINF=`echo - | awk '{print ( '$LIME_GAIN' - 6 ) / 100}'`
     fi
 
     # Turn pilots on if required
@@ -490,7 +497,6 @@ case "$MODE_OUTPUT" in
     # CAL=1 # Calibrate every time
     # CAL=0 # Use saved cal
 
-
     if [ -f "$PATH_LIME_CAL" ]; then                                   # Lime Cal file exists, so read it
       LIMECALFREQ=$(get_config_var limecalfreq $PATH_LIME_CAL)
     else                                                               # No file, so set to calibrate
@@ -514,12 +520,6 @@ case "$MODE_OUTPUT" in
         set_config_var limecalfreq "$FREQ_OUTPUT" $PATH_LIME_CAL       # and reset cal freq
       fi
     fi
-
-
-    # Calculate the exact TS Bitrate for Lime
-    #NEW_BITRATE_TS="$($PATHRPI"/dvb2iq" -s $SYMBOLRATE_K -f $FECNUM"/"$FECDEN \
-    #                  -d -r $UPSAMPLE -m $MODTYPE -c $CONSTLN $PILOTS $FRAMES )"
-
   ;;
 esac
 
